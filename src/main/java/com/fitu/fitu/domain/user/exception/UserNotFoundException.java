122be4ecece31fc11f4f1286310be29165c26049
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-06-09 10:24:50 +0900

    feat(#6): 개인 프로필 등록/수정/조회 API 구현
    * feat(#6): 개인 프로필 등록 API 구현
    
    * feat(#6): 개인 프로필 조회 API 구현
    
    * feat(#6): 개인 프로필 수정 API 구현
    
    * feat(#6): Jpa Auditing Configuration 설정
    
    * feat(#6): Cors 설정
    
    * feat(#6): S3 환경 설정
    
    * feat(#6): 파일 확장자 유효성 검사 클래스 구현
    
    * feat(#6): 전신 사진 유효성 검사 API 구현
    
    * feat(#6): 시스템 에러 처리용 글로벌 예외 클래스 추가
    
    * feat(#6): 전신 사진 S3 업로드 로직 수정
    
    - 이미지 검증 전 temp 디렉토리에 업로드
    - 이미지 검증 및 프로필 등록 완료 시 bodyImage 디렉토리로 이동
    
    * feat(#6): S3 관련 클래스명 S3Uploader으로 수정
    
    * feat(#6): RestClient 환경 설정
    
    * feat(#6): RestClient 사용하여 전신 사진 유효성 검사 ai api와 연동
    
    * feat(#6): @setter 대신 update 메서드 사용
    
    * feat(#6): RestClient 대신 RestTemplate 사용하여 외부 ai api 호출
    
    * feat(#6): user 에러 코드명 수정
    
    * feat(#6): user controller 클래스 어노테이션 순서 변경
    
    * feat(#6): 새로운 전신 사진 등록 시 기존 전신 사진이 있을 경우 삭제 로직 추가

---
+++ src/main/java/com/fitu/fitu/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.fitu.fitu.domain.user.exception;
+
+import com.fitu.fitu.global.error.ErrorCode;
+import com.fitu.fitu.global.error.exception.EntityNotFoundException;
+
+public class UserNotFoundException extends EntityNotFoundException {
+    public UserNotFoundException(final String target) {
+        super(target + " Is Not Found", ErrorCode.USER_NOT_FOUND);
+    }
+}
--- src/main/java/com/fitu/fitu/global/error/ErrorCode.java
+++ src/main/java/com/fitu/fitu/global/error/ErrorCode.java
@@ -8,13 +8,25 @@ public enum ErrorCode {
     /* COMMON ERROR */
     INTERNAL_SERVER_ERROR(500, "COMMON001", "Internal Server Error"),
     INVALID_INPUT_VALUE(400, "COMMON002", "Invalid Input Value"),
-    ENTITY_NOT_FOUND(400, "COMMON003", "Entity Not Found");
+    ENTITY_NOT_FOUND(400, "COMMON003", "Entity Not Found"),
 
     /* USER ERROR */
+    USER_NOT_FOUND(400, "USER001", "User Not Found"),
 
     /* CLOTHES ERROR */
 
     /* RECOMMENDATION ERROR */
+
+    /* S3 ERROR */
+    S3_UPLOAD_FAILED(500, "S3001", "Failed to upload file to S3"),
+    S3_DELETE_FAILED(500, "S3002", "Failed to delete file from S3"),
+    S3_COPY_FAILED(500, "S3003", "Failed to copy file from S3"),
+    S3_INVALID_URL(400, "S3004", "Invalid S3 file URL"),
+
+    /* File ERROR */
+    FILE_NOT_FOUND(400, "FILE001", "File Not Found"),
+    FILE_INVALID_EXTENSION(400, "FILE002", "Invalid File Extension");
+
 
     private final int status;
     private final String code;
